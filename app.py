0b559b281ffbc241f37678a0b4724a503fa66276
zishaweisui <angellevinas@mail.ru>
2023-05-02 18:24:49 +0300

    the third part of tutorial

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import render_template
-import connexion
+import config
+from models import Person
 
-app = connexion.App(__name__, specification_dir="./")
-app.add_api("swagger.yml")
+app = config.connex_app
+app.add_api(config.basedir / "swagger.yml")
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    people = Person.query.all()
+    return render_template("home.html", people=people)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
