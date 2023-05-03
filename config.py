899959c82f054fe0447d24fc444e5312717e5ebd
zishaweisui <angellevinas@mail.ru>
2023-05-03 17:20:34 +0300

    change routes for individual users' pages from lname to person_id

--- config.py
+++ config.py
@@ -4,11 +4,16 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
 basedir = pathlib.Path(__file__).parent.resolve()
+# Create the Connexion application instance
 connex_app = connexion.App(__name__, specification_dir=basedir)
 
+# Configure the SQLAlchemy part of the app instance
 app = connex_app.app
 app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{basedir / 'people.db'}"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+# Create the SQLAlchemy db instance
 db = SQLAlchemy(app)
+
+# Initialize Marshmallow
 ma = Marshmallow(app)
--- people.py
+++ people.py
@@ -18,18 +18,18 @@ def create(person):
     else:
         abort(406, f"Person with last name {lname} already exists")
 
-def read_one(lname):
-    person = Person.query.filter(Person.lname == lname).one_or_none()
+def read_one(person_id):
+    person = Person.query.filter(Person.id == person_id).one_or_none()
 
     if person is not None:
         return person_schema.dump(person)
     else:
         abort(
-            404, f"Person with last name {lname} not found"
+            404, f"The user id not found"
         )
 
-def update(lname, person):
-    existing_person = Person.query.filter(Person.lname == lname).one_or_none()
+def update(person_id, person):
+    existing_person = Person.query.filter(Person.id == person_id).one_or_none()
 
     if existing_person:
         update_person = person_schema.load(person, session=db.session)
@@ -38,14 +38,14 @@ def update(lname, person):
         db.session.commit()
         return person_schema.dump(existing_person), 201
     else:
-        abort(404, f"Person with last name {lname} not found")
+        abort(404, f"The user id not found")
 
-def delete(lname):
-    existing_person = Person.query.filter(Person.lname == lname).one_or_none()
+def delete(person_id):
+    existing_person = Person.query.filter(Person.id == person_id).one_or_none()
 
     if existing_person:
         db.session.delete(existing_person)
         db.session.commit()
-        return make_response(f"{lname} successfully deleted", 200)
+        return make_response(f"Person successfully deleted", 200)
     else:
-        abort(404, f"Person with last name {lname} not found")
+        abort(404, f"The user id not found")
